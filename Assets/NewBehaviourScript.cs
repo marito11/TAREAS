d3f68d0d34429556ac676c6514249d7df2607663
marito11 <barcelona200101@gmail.com>
2022-06-05 10:10:35 -0500

    practica texto terminada

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public TextMeshProUGUI texto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            texto.text= "Es de Pistache";
+        }
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            texto.text= "Es de Chicle";
+        }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            texto.text= "Es de Chocolate";
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            texto.text= "Es de Frambueza";
+        }
+    }
+}
