1f373b4f32663fcd6c46d2e67702048bfe3e2052
marito11 <106764053+marito11@users.noreply.github.com>
2022-06-12 19:20:25 -0500

    se agrego script lenght

---
+++ Script lenght EFE.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reproductor : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioS;
+    public AudioClip[] ListaAudio;
+    public int indice = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+        //ListaAudio =new AudioClip[3];
+
+        audioS.clip = ListaAudio[indice];
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            indice++;
+        }
+         for (int j = 0; j < ListaAudio.Length; j++)
+         {
+                for (int i = 0; i < ListaAudio.Length; i++){
+                    if (ListaAudio[indice] == ListaAudio[indice]){
+                        indice++;
+                    }
+                }
+         }
+
+        audioS.clip = ListaAudio[indice];
+    }
+}
